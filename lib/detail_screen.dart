06c4d8851dadfc460b3618eebd6fa77f0d7c73d5
andrew <iniandrewrp@gmail.com>
2022-08-04 01:49:13 +0700

    add tourism place, back button & favorite

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:wisatabandung/model/tourism_place.dart';
 
 const informationTextStyle = TextStyle(fontFamily: 'Oxygen');
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
+  final TourismPlace place;
+
+  const DetailScreen({Key? key, required this.place}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +16,35 @@ class DetailScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Image.asset('images/farm-house.jpg'),
+                Stack(
+                  children: <Widget>[
+                    Image.asset(place.imageAsset),
+                    SafeArea(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Colors.grey,
+                                child: IconButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    icon: const Icon(Icons.arrow_back)
+                                ),
+                              ),
+                              const FavoriteButton(),
+                            ],
+                          )
+                        )
+                    )
+                  ],
+                ),
                 Container(
                   margin: const EdgeInsets.only(top: 16.0),
-                  child: const Text(
-                    'Farm House Lembang',
+                  child: Text(
+                    place.name,
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontSize: 30.0,
@@ -32,28 +59,28 @@ class DetailScreen extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
                       Column(
-                        children: const [
+                        children: [
                           Icon(Icons.calendar_today),
                           Text(
-                            'Open Yesterday',
+                            place.openDays,
                             style: informationTextStyle,
                           ),
                         ],
                       ),
                       Column(
-                        children: const [
+                        children: [
                           Icon(Icons.access_time),
                           Text(
-                            '09.00 - 20.00',
+                            place.openTime,
                             style: informationTextStyle,
                           ),
                         ],
                       ),
                       Column(
-                        children: const [
+                        children: [
                           Icon(Icons.monetization_on),
                           Text(
-                            'Rp 25.000',
+                            place.ticketPrice,
                             style: informationTextStyle,
                           ),
                         ],
@@ -63,44 +90,58 @@ class DetailScreen extends StatelessWidget {
                 ),
                 Container(
                   padding: const EdgeInsets.all(16.0),
-                  child: const Text(
-                    'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                  child: Text(
+                    place.description,
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16.0),
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      fontFamily: 'Oxygen',
+                    ),
                   ),
                 ),
                 SizedBox(
                   height: 150,
                   child: ListView(
                     scrollDirection: Axis.horizontal,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
-                        )
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
-                        )
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-                          child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
-                        )
-                      ),
-                    ],
+                    children: place.imageUrls.map((url) {
+                      return Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+                            child: Image.network(url),
+                          )
+                      );
+                    }).toList(),
                   ),
                 )
               ],
             ),
           )
+        )
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          setState(() {
+            isFavorite = !isFavorite;
+          });
+        },
+        icon: Icon(
+          isFavorite ? Icons.favorite : Icons.favorite_border
         )
     );
   }
